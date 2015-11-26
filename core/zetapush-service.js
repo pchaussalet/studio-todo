79158923b07f1b33f0b0561474c30e0f49f63aa3
Pierre Chaussalet <pchaussalet@gmail.com>
2015-11-26 12:50:47 +0000

    Add module zetapush-service

---
+++ core/zetapush-service.js
@@ -0,0 +1,15 @@
+/**
+ * @module core/zetapush-service
+ */
+var Montage = require("montage/core/core").Montage;
+/**
+ * @class ZetapushService
+ * @extends Montage
+ */
+exports.ZetapushService = Montage.specialize(/** @lends ZetapushService# */ {
+    constructor: {
+        value: function ZetapushService() {
+            this.super();
+        }
+    }
+});
