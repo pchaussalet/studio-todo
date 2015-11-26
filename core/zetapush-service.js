e8324d6311824e7fe4ac3f6eb993014017d4aa3d
Pierre Chaussalet <pchaussalet@gmail.com>
2015-11-26 13:45:40 +0000

    Update file core/zetapush-service.js

--- core/zetapush-service.js
+++ core/zetapush-service.js
@@ -25,20 +25,23 @@ exports.ZetapushService = Montage.specialize(/** @lends ZetapushService# */ {
 
     constructor: {
         value: function ZetapushService() {
+            var self = this;
+            
             if (!localStorage['resource']){
                 localStorage['resource']= zp.makeResourceId();
             }
+            
             this.macroService= new zp.service.Generic('Lghx');
             this.stackService= new zp.service.Generic('3vJB');
             this.gdaService= new zp.service.Generic('dX6v');
 
             zp.onConnected(function(msg) {
-                if (authent.getToken()){
-                    localStorage['token']= authent.getToken();
-                    localStorage['publicToken']= authent.getPublicToken();
+                if (self.auth.getToken()){
+                    localStorage['token']= self.auth.getToken();
+                    localStorage['publicToken']= self.auth.getPublicToken();
                 }
-                userId = authent.getUserId();
-                console.log('You are connected with userId ', authent.getUserId());
+                userId = self.auth.getUserId();
+                console.log('You are connected with userId ', self.auth.getUserId());
             });
             this.connect();
         }
