1b567311bc2e58fcc4afbf0f3a292d8a5bc300bb
Pierre Chaussalet <pchaussalet@gmail.com>
2015-11-27 17:15:41 +0000

    Update component ui/main.reel/main.js

--- ui/main.reel/main.js
+++ ui/main.reel/main.js
@@ -41,9 +41,9 @@ exports.Main = Component.specialize(/** @lends Main# */ {
                     });
                     self.zetapushService.registerHandler('update', function(data) {
                         var updatedEntry = self.tasksController.content.filter(function(entry) { return entry.guid && entry.guid === data.guid; })[0];
-                        console.log(updatedEntry);
+                        console.log(updatedEntry.data);
                         updatedEntry.data = data.data;
-                        console.log(updatedEntry);
+                        console.log(updatedEntry.data);
                     });
                 });
             }
