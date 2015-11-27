7db0de23ec48684205c8f21e6405ea24f27807a4
Pierre Chaussalet <pchaussalet@gmail.com>
2015-11-27 15:25:34 +0000

    Update file core/zetapush-service.js

--- core/zetapush-service.js
+++ core/zetapush-service.js
@@ -80,6 +80,7 @@ exports.ZetapushService = Montage.specialize(/** @lends ZetapushService# */ {
             this.macroService.on('listTodoList', function(msg) {
                 var todoListFound = false;
                 if (msg.data && msg.data.result && msg.data.result && msg.data.result.todoList) {
+console.log(msg.data.result.todoList.content);
                     for (var i = 0, todoListLength = msg.data.result.todoList.content.length; i < todoListLength; i++) {
                         if (msg.data.result.todoList.content[i].userName === 'shared-list') {
                             self.ZPTodoList = msg.data.result.todoList.content[i];
