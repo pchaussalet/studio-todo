1990b73ffe166d9c0ca5084d0d9c59d8abcf4653
Pierre Chaussalet <pchaussalet@gmail.com>
2015-11-27 14:18:12 +0000

    Update component ui/main.reel/main.js

--- ui/main.reel/main.js
+++ ui/main.reel/main.js
@@ -22,7 +22,7 @@ exports.Main = Component.specialize(/** @lends Main# */ {
         value: function Main() {
             this.super();
             this.zetapushService = new ZetapushService();
-            this.zetapushService.conect();
+            this.zetapushService.connect();
         }
     },
 
