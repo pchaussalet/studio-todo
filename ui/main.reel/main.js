7a50482d6515425e52f1df221d02ad847be75553
Pierre Chaussalet <pchaussalet@gmail.com>
2015-11-30 13:24:16 +0000

    Update component ui/main.reel/main.js

--- ui/main.reel/main.js
+++ ui/main.reel/main.js
@@ -57,7 +57,7 @@ exports.Main = Component.specialize(/** @lends Main# */ {
                         console.log(index);
                         if (index > -1) {
                             self.todos.splice(index, 0 ,todo);
-                            self.todos.splice(index, 1);
+//                            self.todos.splice(index, 1);
                             console.log(self.todos[index].data);
                             self.needsDraw = true;
                         }
